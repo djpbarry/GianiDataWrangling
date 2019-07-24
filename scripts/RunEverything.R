28c6b6bcd73758b2d3e78130bfaa0e3b5a1a8ec8
Dave Barry <david.barry@crick.ac.uk>
2019-07-24 15:46:06 +0100

    Refactoring to tidy up code

---
+++ scripts/RunEverything.R
@@ -0,0 +1,5 @@
+source("scripts/DataPreProcessing.R");
+source("scripts/cellCount.R");
+source("scripts/CellMorphology.R");
+source("scripts/DistanceVersusMorphology.R");
+source("scripts/DistanceVersusExpression.R");
